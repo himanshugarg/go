29e3732d66d7b302d4496904c9258864d4ff491f
himanshugarg <Himanshu Garg>
2022-04-05 06:55:50 +0530

    Exerises 1.7 and 1.9

--- dup.go
+++ dup.go
@@ -1,31 +1,37 @@
-// Prints the count and text of lines that appear more than once 
-// in the list of named files
-
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "os"
-  "strings"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func main() {
-  counts := make(map[string]int)
-  for _, filename := range os.Args[1:] {
-    data, err := ioutil.ReadFile(filename)
-    if err != nil {
-      fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
-      continue
-    }
-    for _, line := range strings.Split(string(data), "\n") {
-      counts[line]++
-    }
-  }
+	counts := make(map[string]int)
+	files := make(map[string]map[string]bool)
 
-  for line, n := range counts {
-    if n > 1 {
-      fmt.Printf("%d\t%s\n", n, line)
-    }
-  }
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+			if files[line] == nil {
+				files[line] = make(map[string]bool)
+			}
+			files[line][filename] = true
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+			for file, _ := range files[line] {
+				fmt.Printf("%s ", file);
+			}
+		}
+	}
 }
